cabebc1b20f2549fbc54ed5f3c06cbbece25d05b
dilipsun <dilip0527@gmail.com>
2026-07-06 14:03:15 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "root_management_group_id" {
-  description = "Root management group ID. Usually same as tenant ID."
+  description = "Root management group ID. Usually same as tenant ID"
   type        = string
   default     = ""
 }
 
 variable "principal_object_id" {
-  description = "Object ID (user/group/service principal) to receive custom role assignment."
+  description = "Object ID (user/group/service principal) to receive custom role assignment"
   type        = string
 }
